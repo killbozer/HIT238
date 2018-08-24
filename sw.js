9bd6d42b048f2a13fd91becc2c1d954d32682d15
Matthew James Wheelhouse <S301012@students.cdu.edu.au>
2018-08-24 14:29:14 +0930

    fix sw items

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ var CACHE_TITLE = 'My-Cache';
 var CACHE_VERSION = 'v1';
 var CACHE_NAME = CACHE_TITLE + '-' + 'CACHE_VERSION';
 var urlsToCache = [
-  '/',  
-  '/styles/main.css',
-  '/script/main.js'
+  'index.html',  
+  'styles.css',
+  'scripts.js'
 ];
 
 self.addEventListener('install', function(event) {
