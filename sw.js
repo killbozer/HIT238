846d99baec20858e54f00a0148de36c13a406ec7
Matthew James Wheelhouse <S301012@students.cdu.edu.au>
2018-08-24 15:06:10 +0930

    spacing

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
+// Perform install steps
+	event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
@@ -19,14 +19,14 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
+	event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+    .then(function(response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
+    return fetch(event.request);
       }
     )
   );
