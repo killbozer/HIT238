95685aa9293d2401b637296b6dc114228719abc6
Matthew James Wheelhouse <S301012@students.cdu.edu.au>
2018-08-24 14:14:13 +0930

    added sw

---
+++ sw.js
@@ -0,0 +1,17 @@
+var CACHE_TITLE = 'My-Cache';
+var CACHE_VERSION = 'v1';
+var CACHE_NAME = CACHE_TITLE + '-' + 'CACHE_VERSION';
+var urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
